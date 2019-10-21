37b5e8a78e9d03199004e7557b69a953a4c2b01d
Audronf <federicofasitella@gmail.com>
2019-10-21 11:33:37 -0300

    .

--- solver.py
+++ solver.py
@@ -2,12 +2,12 @@ import random
 import copy
 import time
 
-n_population = 100000
-liveness = 200
-mutants = 200
-fail_puntos = 0.5
-punish_puntos = 1
-liveness_probability = 70
+n_poblacion = 100000
+n_sobreviven_cruzamiento = 200
+probabilidad_sobrevivir_cruzamiento = 70
+n_mutantes = 200
+puntos_por_fallar = 0.5
+puntos_castigo = 1
 
 colores = 		["roja","verde", "negra", "amarilla", "azul"]
 profesiones = 	["matematico", "hacker", "analista", "desarrollador", "ingeniero"]
@@ -22,20 +22,20 @@ class matriz:
 	def __init__(self):
 		self.matriz = [[0 for x in range(5)] for x in range(5)] 
 		self.puntos = 20
-		self.approve = 0
+		self.restriccionesCumplidas = 0
 		self.noCumplidas = []
 
 	def getmatriz(self, x, y):
 		return self.matriz[x][y]
 
-	def randFill(self):
+	def llenarMatrizRandom(self):
 		for x in range(0,5):
 			for y in range(0,5):
 				self.matriz[x][y] = random.sample(matrizProto[x], 1)[0] 
 				pass
 			pass
 
-	def mutate(self):
+	def mutar(self):
 		variablesY = [0,1,2,3,4]
 		x  = random.randint(0,4)
 		y1 = random.choice(variablesY)
@@ -45,277 +45,260 @@ class matriz:
 		self.matriz[x][y1] = self.matriz[x][y2]
 		self.matriz[x][y2] = temp
 
-	def test(self):
-
+	def prueba(self):
 		# El matemático vive en la casa roja
 		try:
 			i = self.matriz[1].index('matematico')
 			if self.matriz[0][i] == 'roja':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El matemático vive en la casa roja")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El hacker programa en python
 		try:
 			i = self.matriz[1].index('hacker')
 			if self.matriz[2][i] == 'python':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
-				self.noCumplidas.append("El hacker programa en python")
-				
+				self.puntos -= puntos_por_fallar
+				self.noCumplidas.append("El hacker programa en python")	
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que vive en la casa verde codea en vscode
 		try:
 			i = self.matriz[0].index('verde')
 			if self.matriz[3][i] == 'vscode':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que vive en la casa verde codea en vscode")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El analista usa Atom
 		try:
 			i = self.matriz[1].index('analista')
 			if self.matriz[3][i] == 'atom':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El analista usa Atom")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que vive en la casa verde está a la derecha del de la casa negra
 		try:
 			i = self.matriz[0].index('verde')
 			if self.matriz[0][i-1] == 'negra' and i != 0:
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que vive en la casa verde está a la derecha del de la casa negra")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que usa Redis codea en java
 		try:
 			i = self.matriz[4].index('redis')
 			if self.matriz[2][i] == 'java':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que usa Redis codea en java")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que usa Cassandra vive en la casa amarilla
 		try:
 			i = self.matriz[4].index('cassandra')
 			if self.matriz[0][i] == 'amarilla':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que usa Cassandra vive en la casa amarilla")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que usa notepad++ vive en la casa del medio
 		try:
 			if self.matriz[3][2] == 'notepad++':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que usa notepad++ vive en la casa del medio")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 		
 		# El desarrollador vive en la primera casa
 		try:
 			if self.matriz[1][0] == 'desarrollador':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El desarrollador vive en la primera casa")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que usa hadoop vive al lado del que codea javascript
 		try:	
 			i = self.matriz[4].index('hadoop')
 			if (self.matriz[2][i-1] == 'javascript' and i != 0) or (self.matriz[2][i+1] == 'javascript' and i != 4):
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que usa hadoop vive al lado del de js")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que programa en c# vive al lado del que usa cassandra
 		try:
 			i = self.matriz[2].index('c#')
 			if (self.matriz[4][i+1] == 'cassandra' and i != 4) or (self.matriz[4][i-1] == 'cassandra' and i != 0):
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que programa en c# vive al lado del que usa cassandra")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El que usa Neo4J usa sublime
 		try:
 			i = self.matriz[4].index('neo4j')
 			if self.matriz[3][i] == 'sublime':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El que usa Neo4J usa sublime")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El ingeniero usa mongo
 		try:
 			i = self.matriz[1].index('ingeniero')
 			if self.matriz[4][i] == 'mongo':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El ingeniero usa mongodb")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El desarrollador vive al lado del de la casa azul
 		try:
 			i = self.matriz[0].index('azul')
 			if (self.matriz[1][i+1] == 'desarrollador' and i != 4) or (self.matriz[1][i-1] == 'desarrollador' and i != 0):
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El desarrollador vive al lado del de la casa azul")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
 		# El programador de c++ lo hace en vim
 		try:
 			i = self.matriz[2].index('c++')
 			if self.matriz[3][i] == 'vim':
 				self.puntos += 1
-				self.approve += 1
+				self.restriccionesCumplidas += 1
 			else:
-				self.puntos -= fail_puntos
+				self.puntos -= puntos_por_fallar
 				self.noCumplidas.append("El programador de c++ lo hace en vim")
 		except:
-			self.puntos -= punish_puntos
+			self.puntos -= puntos_castigo
 
-		#print(self.matriz)
-		#print(self.puntos)
-
-class Puzzle:
+class AcertijoEinstein:
 
 	def __init__(self):
-		 self.population = []
+		 self.poblacion = []
 
-	def solve(self):
-
-		self.generate(n_population)
+	def resolucion(self):
+		self.generacion(n_poblacion)
 		x = 0
-
 		while True:
 			x += 1
 			print('Iteration  %d' %x)
-			self.test()
-			approve =  self.population[0].approve
-			if approve >= 15:
+			self.prueba()
+			restriccionesCumplidas =  self.poblacion[0].restriccionesCumplidas
+			if restriccionesCumplidas >= 15:
 				break
-			self.crossOver(liveness, n_population)
-			self.mutate()
-			
+			self.cruzamiento(n_sobreviven_cruzamiento, n_poblacion)
+			self.mutar()	
+			pass
+		self.prueba()
+		print(self.poblacion[0].matriz)
+		print(self.poblacion[0].restriccionesCumplidas)
+
+	def mutar(self):
+		for x in range(0,n_mutantes):
+			y = random.randint(0,len(self.poblacion)-1)
+			self.poblacion[y].mutar()
 			pass
 
-		self.test()
-		print(self.population[0].matriz)
-		print(self.population[0].approve)
-
-	def mutate(self):
-		for x in range(0,mutants):
-			y = random.randint(0,len(self.population)-1)
-			self.population[y].mutate()
-			pass
-		
-
-	def generate(self, i):
+	def generacion(self, i):
 		for x in range(0,i):
-			newborn = matriz()
-			newborn.randFill()
-			self.population.append(newborn)
+			nuevo_individuo = matriz()
+			nuevo_individuo.llenarMatrizRandom()
+			self.poblacion.append(nuevo_individuo)
 			pass
 
-	def crossOver(self, i, limit):
-		
-		goodPopulation = []
+	def cruzamiento(self, i, limit):
+		goodpoblacion = []
 		i = 0
-		while len(goodPopulation)<liveness:
-			if random.randint(0,100)<liveness_probability:
-				goodPopulation.append(self.population[i])
+		while len(goodpoblacion)<n_sobreviven_cruzamiento:
+			if random.randint(0,100)<probabilidad_sobrevivir_cruzamiento:
+				goodpoblacion.append(self.poblacion[i])
 			i += 1 
-			i %= len(self.population)
-
+			i %= len(self.poblacion)
 		newGeneration = []
 		while len(newGeneration) <= limit:
-			first = goodPopulation[random.randint(0,len(goodPopulation)-1)]
-			second = goodPopulation[random.randint(0,len(goodPopulation)-1)]
-			third = goodPopulation[random.randint(0,len(goodPopulation)-1)]
-			newborn = self.cross(first, second, third)
-			newGeneration.append(newborn)
+			primero = goodpoblacion[random.randint(0,len(goodpoblacion)-1)]
+			segundo = goodpoblacion[random.randint(0,len(goodpoblacion)-1)]
+			tercero = goodpoblacion[random.randint(0,len(goodpoblacion)-1)]
+			nuevo_individuo = self.cruza(primero, segundo, tercero)
+			newGeneration.append(nuevo_individuo)
+		self.poblacion = newGeneration
 
-		self.population = newGeneration
-
-	def cross(self, first, second, third):
-		newborn = matriz()
-		#newborn.randFill()
+	def cruza(self, primero, segundo, tercero):
+		nuevo_individuo = matriz()
 		for x in range(0,5):
 			for y in range(0,5):
-
 				i = random.randint(0,2)
 				if i == 0:
-					newborn.matriz[x][y] = first.getmatriz(x,y)
+					nuevo_individuo.matriz[x][y] = primero.getmatriz(x,y)
 				elif i == 1:
-					newborn.matriz[x][y] = second.getmatriz(x,y)
+					nuevo_individuo.matriz[x][y] = segundo.getmatriz(x,y)
 				else:
-					newborn.matriz[x][y] = third.getmatriz(x,y)
+					nuevo_individuo.matriz[x][y] = tercero.getmatriz(x,y)
 				pass
 			pass
-		return newborn
+		return nuevo_individuo
 
-	def test(self):
-		for x in range(0,len(self.population)):
-			self.population[x].test()
+	def prueba(self):
+		for x in range(0,len(self.poblacion)):
+			self.poblacion[x].prueba()
 			pass
+		self.poblacion.sort(key=lambda x: x.puntos, reverse=True)
+		print(self.poblacion[0].restriccionesCumplidas)
+		print(self.poblacion[0].matriz)
+		print(self.poblacion[0].noCumplidas)
 
-		self.population.sort(key=lambda x: x.puntos, reverse=True)
-		print(self.population[0].approve)
-		print(self.population[0].matriz)
-		print(self.population[0].noCumplidas)
-
-
-puz = Puzzle()
-puz.solve()
+puz = AcertijoEinstein()
+puz.resolucion()
